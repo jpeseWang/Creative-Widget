ac039270c8b4375edff8e555eea32a9df6ba5499
Linh Nguyen <linh.hoang.nguyen@enlabsoftware.com>
2023-04-13 00:53:39 +0700

    fix: authen

--- libs/shared/model/src/response-model/user.model.ts
+++ libs/shared/model/src/response-model/user.model.ts
@@ -3,6 +3,8 @@ export class UserProfileModel {
   id?: string;
   email!: string;
   role!: string;
+
+  userAvatarUrl?: string;
   public constructor(init?: Partial<UserProfileModel>) {
     Object.assign(this, init);
   }
