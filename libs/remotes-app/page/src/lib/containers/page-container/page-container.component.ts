91ebe9a376c67456ce5a57581a58f79ee28d5aa6
Linh Nguyen <linh.hoang.nguyen@enlabsoftware.com>
2023-04-12 20:19:12 +0700

    feat: implement page template

--- libs/remotes-app/page/src/lib/containers/page-container/page-container.component.ts
+++ libs/remotes-app/page/src/lib/containers/page-container/page-container.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+interface Document {
+  id: number;
+  title: string;
+}
 
 @Component({
   selector: 'cwp-page-container',
   templateUrl: './page-container.component.html',
   styleUrls: ['./page-container.component.scss'],
 })
-export class PageContainerComponent {}
+export class PageContainerComponent {
+  documents: Document[];
+  constructor() {
+    // Mock data for documents
+    this.documents = [
+      { id: 1, title: 'Document 1' },
+      { id: 2, title: 'Document 2' },
+      { id: 3, title: 'Document 3' },
+      { id: 4, title: 'Document 4' },
+      // Add more documents as needed
+    ];
+  }
+}
